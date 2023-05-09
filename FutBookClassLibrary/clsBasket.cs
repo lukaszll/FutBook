c87fee3c565b5d4d41cc91ada12c29fa858152e4
Lukasz Lipka <P2624016@my365.dmu.ac.uk>
2023-05-09 18:28:47 +0100

    Basket functionality added

---
+++ FutBookClassLibrary/clsBasket.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutBookClassLibrary
+{
+    public class clsBasket
+    {
+
+        /// this class defines some typical attributes of a shopping basket
+
+
+        List<clsBasketItem> mProducts = new List<clsBasketItem>();
+        public clsBasket()
+        {
+
+        }
+
+        private Int32 mAccountNo;
+        public Int32 AccountNo
+        {
+            get
+            {
+                return mAccountNo;
+            }
+            set
+            {
+                mAccountNo = value;
+            }
+        }
+
+        private string mCardNo;
+        public string CardNo
+        {
+            get
+            {
+                return mCardNo;
+            }
+            set
+            {
+                mCardNo = value;
+            }
+        }
+
+        public List<clsBasketItem> Products
+        {
+            get
+            {
+                return mProducts;
+            }
+        }
+
+        public void Checkout()
+        {
+            //create an instance of the order class
+            clsOrder Order = new clsOrder();
+            //invoke the ProcessCart method
+            Order.ProcessCart(this);
+        }
+    }
+}
