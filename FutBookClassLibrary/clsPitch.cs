dc7870801a972276e48a7fd7e26a7c7e5c056c14
P2611172 <P2611172@GWHXX281WIN017W>
2023-05-09 15:06:34 +0100

    hi

---
+++ FutBookClassLibrary/clsPitch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutBookClassLibrary
+{
+    class clsPitch
+    {        
+        private int mPitchNo;
+        private string mPitchName;
+        public int PitchNo
+        {
+            get { return mPitchNo; }
+            set { mPitchNo = value; }
+        }
+
+        public string PitchName
+        {
+            get { return mPitchName; }
+            set { mPitchName = value; }
+        }
+    }
+}
