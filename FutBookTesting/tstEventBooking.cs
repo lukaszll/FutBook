fe885d0b854835b54253f47cd81e5bb9f96b3e63
p2577974 <p2577974@my365.dmu.ac.uk>
2023-05-11 20:06:22 +0100

    added some testing, needs improvement

---
+++ FutBookTesting/tstEventBooking.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FutBookClassLibrary;
+using System;
+
+namespace FutBookTesting
+{
+    [TestClass]
+    public class clsEventBookingTests
+    {
+        [TestMethod]
+        public void TestValid()
+        {
+            // Arrange
+            clsEventBooking booking = new clsEventBooking();
+            string eventName = "Test Event";
+            string eventDate = DateTime.Now.AddDays(1).ToString();
+            string specialRequests = "Test Request";
+            string numParticipants = "4";
+
+            // Act
+            string result = booking.Valid(eventName, eventDate, specialRequests, numParticipants);
+
+            // Assert
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void TestValidInvalidDate()
+        {
+            // Arrange
+            clsEventBooking booking = new clsEventBooking();
+            string eventName = "Test Event";
+            string eventDate = "invalid date";
+            string specialRequests = "Test Request";
+            string numParticipants = "4";
+
+            // Act
+            string result = booking.Valid(eventName, eventDate, specialRequests, numParticipants);
+
+            // Assert
+            Assert.AreEqual("The EventDate must be a valid date. ", result);
+        }
+
+    }
+}
