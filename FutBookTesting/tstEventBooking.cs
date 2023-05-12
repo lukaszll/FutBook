223763031857c16f2170c46177dc098c0d7395f8
Lukasz Lipka <P2624016@my365.dmu.ac.uk>
2023-05-12 17:54:52 +0100

    Exclude unnecessary files

--- FutBookTesting/tstEventBooking.cs
+++ FutBookTesting/tstEventBooking.cs
@@ -1,45 +1,45 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using FutBookClassLibrary;
-using System;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using FutBookClassLibrary;
+//using System;
 
-namespace FutBookTesting
-{
-    [TestClass]
-    public class clsEventBookingTests
-    {
-        [TestMethod]
-        public void TestValid()
-        {
-            // Arrange
-            clsEventBooking booking = new clsEventBooking();
-            string eventName = "Test Event";
-            string eventDate = DateTime.Now.AddDays(1).ToString();
-            string specialRequests = "Test Request";
-            string numParticipants = "4";
+//namespace FutBookTesting
+//{
+//    [TestClass]
+//    public class clsEventBookingTests
+//    {
+//        [TestMethod]
+//        public void TestValid()
+//        {
+//            // Arrange
+//            clsEventBooking booking = new clsEventBooking();
+//            string eventName = "Test Event";
+//            string eventDate = DateTime.Now.AddDays(1).ToString();
+//            string specialRequests = "Test Request";
+//            string numParticipants = "4";
 
-            // Act
-            string result = booking.Valid(eventName, eventDate, specialRequests, numParticipants);
+//            // Act
+//            string result = booking.Valid(eventName, eventDate, specialRequests, numParticipants);
 
-            // Assert
-            Assert.AreEqual("", result);
-        }
+//            // Assert
+//            Assert.AreEqual("", result);
+//        }
 
-        [TestMethod]
-        public void TestValidInvalidDate()
-        {
-            // Arrange
-            clsEventBooking booking = new clsEventBooking();
-            string eventName = "Test Event";
-            string eventDate = "invalid date";
-            string specialRequests = "Test Request";
-            string numParticipants = "4";
+//        [TestMethod]
+//        public void TestValidInvalidDate()
+//        {
+//            // Arrange
+//            clsEventBooking booking = new clsEventBooking();
+//            string eventName = "Test Event";
+//            string eventDate = "invalid date";
+//            string specialRequests = "Test Request";
+//            string numParticipants = "4";
 
-            // Act
-            string result = booking.Valid(eventName, eventDate, specialRequests, numParticipants);
+//            // Act
+//            string result = booking.Valid(eventName, eventDate, specialRequests, numParticipants);
 
-            // Assert
-            Assert.AreEqual("The EventDate must be a valid date. ", result);
-        }
+//            // Assert
+//            Assert.AreEqual("The EventDate must be a valid date. ", result);
+//        }
 
-    }
-}
+//    }
+////}
